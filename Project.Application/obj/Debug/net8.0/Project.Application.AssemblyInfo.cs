149af9a445f91a5af8230099ed36498e7220ecc9
Selçuk TUNÇER <smtuncer123@gmail.com>
2024-09-10 12:48:44 +0300

    g

--- Project.Application/obj/Debug/net8.0/Project.Application.AssemblyInfo.cs
+++ Project.Application/obj/Debug/net8.0/Project.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8c602ed8c5861137f500b42c87be99035e91d08")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df6adb6b5e24b11c376e8cc44fbe83285b96ff3d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
